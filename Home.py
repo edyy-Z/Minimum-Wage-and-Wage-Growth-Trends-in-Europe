3a803f239efe4109c33ac26e9954a53db1821124
Yongjun Zhu <eddiezhuyongjun@hotmail.com>
2025-04-30 02:26:08 +0000

    delete background image

--- Home.py
+++ Home.py
@@ -1,24 +1,5 @@
 import streamlit as st, base64, pathlib
 
-st.set_page_config(page_title="Background-image demo")
-
-img_path = pathlib.Path("image.jpeg")     
-b64_img   = base64.b64encode(img_path.read_bytes()).decode()
-
-st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url("data:image/jpeg;base64,{b64_img}");
-        background-size: cover;            /* fill screen without distortion */
-        background-attachment: fixed;      /* optional parallax effect      */
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-
 
 st.title("Minimum Wage and Wage Growth Trends in Europe")
 st.markdown(
